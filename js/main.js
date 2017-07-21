7e5273691b28b1bd952891ad52d927dd69a028e5
yosef <yosefmiller613@gmail.com>
2017-07-21 15:31:06 -0400

    Improving navigation bars

--- js/main.js
+++ js/main.js
@@ -20,16 +20,22 @@ $(document).ready(function () {
 	
 	/* Sub-site Navigation */
 	var NavTimeout;
-	$("#nasa__sub-navigation").find(".nav li a").hover(function () {
+	$("#nasa__sub-navigation").find(".nav li a").on("mouseenter touch", function (e) {
+		// Display dropdown on hover or touch
 		var nav_class = $(this).prop("class");
 		if (nav_class) {
-			$(".nasa__sub-navigation-dropdown:not(."+nav_class+")").stop(true, true).css({"display": "none", "opacity": 0}); //removeClass("active");
-			$(".nasa__sub-navigation-dropdown."+nav_class+"").slideDown().css({ opacity: 1, transition: 'opacity 0.5s' }); //.addClass("active");
+			// Cancel any current animations and begin a new animation
 			clearTimeout(NavTimeout);
+			$(".nasa__sub-navigation-dropdown:not(."+nav_class+")").stop(true, true).css({"display": "none", "opacity": 0});
+			$(".nasa__sub-navigation-dropdown."+nav_class+"").slideDown().css({ opacity: 1, transition: 'opacity 0.5s' });
 		}
+	}).on("touchend", function (e) {
+		// Treat as dropdown on touch devices, not as a link
+		e.preventDefault();
+		return false;
 	});
 	$(".nasa__sub-navigation-dropdown, #nasa__sub-navigation .nav li a").mouseleave(function () {
-		NavTimeout = setTimeout(function () { $(".nasa__sub-navigation-dropdown").css({ opacity: 0, transition: 'opacity 0.5s' }).slideUp(); /*removeClass("active");*/ }, 100);
+		NavTimeout = setTimeout(function () { $(".nasa__sub-navigation-dropdown").css({ opacity: 0, transition: 'opacity 0.5s' }).slideUp(); }, 100);
 	});
 	$(".nasa__sub-navigation-dropdown").mouseover(function () {
 		clearTimeout(NavTimeout);
